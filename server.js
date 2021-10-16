aefa67d0131c6629cd1d52b26d1699cc1be5e91c
SCiytcor <carlos.morag@estudiantes.uo.edu.cu>
2021-10-15 23:33:48 -0400

    added cors morgan to server

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ const logger = require('morgan');
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(logger('dev'))
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
